80c58b0a706acabedbb9cc36db168ce4508ce5d0
thepetk <thepetk@gmail.com>
2024-12-28 22:46:20 +0000

    Iterate over models

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ from torchvision import transforms
 
 from sfd40 import Stanford40DataSplitter, load_data, get_hyperparameters
 from models import PretrainedNN, CustomActionRecogntionNN, validate, train, test
+import os
+
+MODEL = os.getenv("MODEL", "")
+
+
+class ModelChoice:
+    RESNET = "pretrained"
+    CUSTOM = "custom"
+
+
+def get_chosen_models(model: "str" = MODEL) -> "list[str]":
+    if model == ModelChoice.CUSTOM:
+        print("Model:: Only custom model is included")
+        return [ModelChoice.CUSTOM]
+    elif model == ModelChoice.RESNET:
+        print("Model:: Only resnet model is included")
+        return [ModelChoice.RESNET]
+    else:
+        print("Model:: Both models are included")
+        return [ModelChoice.CUSTOM, ModelChoice.RESNET]
+
+
+CHOSEN_MODELS = get_chosen_models()
 
 
 def main() -> "None":
@@ -30,30 +53,46 @@ def main() -> "None":
         labels, train_items, test_items, validation_items, transform, hparams
     )
 
-    print("\n", "-" * 8, "Initializing NN", "-" * 8)
-    model = CustomActionRecogntionNN(
-        hparams.in_channels, stanford_loader.num_classes, hparams.resize
-    ).to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=hparams.learning_rate)
-    print("Model:: Initialized NN, optimizer and criterion")
-
-    print("\n", "-" * 8, "Model Training and Validation", "-" * 8)
-    for epoch in range(hparams.num_epochs):
-        model, loss = train(model, stanford_loader.train, device, criterion, optimizer)
-        print(f"Epoch {epoch + 1}/{hparams.num_epochs}, Training Loss: {loss:.4f}")
-
-        model, avg_val_loss = validate(
-            model, stanford_loader.validation, device, criterion
-        )
+    for idx, model_name in enumerate(CHOSEN_MODELS):
         print(
-            f"Epoch {epoch + 1}/{hparams.num_epochs}, Validation Loss: {avg_val_loss:.4f}"
+            "\n",
+            "-" * 8,
+            f"[{idx+1}/{len(CHOSEN_MODELS)}] Initializing {model_name} NN",
+            "-" * 8,
         )
+        if model_name == ModelChoice.CUSTOM:
+            model = CustomActionRecogntionNN(
+                hparams.in_channels, stanford_loader.num_classes, hparams.resize
+            ).to(device)
+        else:
+            model = PretrainedNN(hparams.in_channels, stanford_loader.num_classes)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=hparams.learning_rate)
+        print(f"Model:: Initialized {model_name} NN, optimizer and criterion")
 
-    avg_test_loss = test(model, stanford_loader.test, device, criterion)
-    print(f"Test Loss: {avg_test_loss:.4f}")
+        print(
+            "\n",
+            "-" * 8,
+            f"[{idx+1}/{len(CHOSEN_MODELS)}] Model Training and Validation",
+            "-" * 8,
+        )
+        for epoch in range(hparams.num_epochs):
+            model, loss = train(
+                model, stanford_loader.train, device, criterion, optimizer
+            )
+            print(f"Epoch {epoch + 1}/{hparams.num_epochs}, Training Loss: {loss:.4f}")
 
-    torch.save(model.state_dict(), "segmentation_model.pth")
+            model, avg_val_loss = validate(
+                model, stanford_loader.validation, device, criterion
+            )
+            print(
+                f"Epoch {epoch + 1}/{hparams.num_epochs}, Validation Loss: {avg_val_loss:.4f}"
+            )
+
+        avg_test_loss, accuracy = test(model, stanford_loader.test, device, criterion)
+        print(f"Test Loss: {avg_test_loss:.4f} | Accuracy: {accuracy:.2f}%")
+
+        torch.save(model.state_dict(), f"segmentation_{model_name}_.pth")
 
 
 if __name__ == "__main__":
--- models/pretrained_nn.py
+++ models/pretrained_nn.py
@@ -3,16 +3,16 @@ from torchvision import models
 
 
 class PretrainedNN(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: "int", num_classes: "int"):
         super(PretrainedNN, self).__init__()
-        self.resnet = models.resnet18(pretrained=True)
+        self.resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         self.resnet.conv1 = nn.Conv2d(
-            1, 64, kernel_size=7, stride=2, padding=3, bias=False
+            in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
         )
         self.resnet.conv2 = nn.Conv2d(
             64, 128, kernel_size=5, stride=2, padding=3, bias=False
         )
-        self.resnet.fc = nn.Linear(512, 10)
+        self.resnet.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
         return self.resnet(x)
