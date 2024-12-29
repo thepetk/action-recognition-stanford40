Fix loss calculation

models/utils.py
@@ -13,17 +13,20 @@ def train(
     optimizer: "optim.Adam",
 ) -> "tuple[CustomActionRecogntionNN, float]":
     model.train()
+    total_loss = 0.0
     for inputs, targets in train_loader:
         inputs, targets = inputs.to(device), targets.to(device)
         # Forward pass
         outputs = model(inputs)
         # Compute loss
         loss = criterion(outputs, targets)
+        total_loss += loss.item()
         # Backward pass and optimization
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    return model, loss.item()
+    avg_train_loss = total_loss / len(train_loader)
+    return model, avg_train_loss
 
 
 def test(
@@ -36,18 +39,19 @@ def test(
     model.eval()
     correct = 0
     total = 0
-    test_loss = 0.0
+    total_loss = 0.0
     with torch.no_grad():
         for test_inputs, test_targets in loader:
             test_inputs, test_targets = test_inputs.to(device), test_targets.to(device)
             outputs = model(test_inputs)
-            test_loss += criterion(outputs, test_targets)
+            test_loss = criterion(outputs, test_targets)
+            total_loss += test_loss.item()
             _, predicted = torch.max(outputs.data, 1)
             total += test_targets.size(0)
             correct += (predicted == test_targets).sum().item()
 
     accuracy = 100 * correct / total
-    avg_test_loss = test_loss / len(loader)
+    avg_test_loss = total_loss / len(loader)
     return avg_test_loss, accuracy
 
 
@@ -59,11 +63,12 @@ def validate(
 ) -> "tuple[CustomActionRecogntionNN, float]":
     model.eval()
     with torch.no_grad():
-        val_loss = 0.0
+        total_loss = 0.0
         for val_inputs, val_targets in loader:
             val_inputs, val_targets = val_inputs.to(device), val_targets.to(device)
             val_outputs = model(val_inputs)
-            val_loss += criterion(val_outputs, val_targets)
+            val_loss = criterion(val_outputs, val_targets)
+            total_loss += val_loss
 
-        avg_val_loss = val_loss / len(loader)
+        avg_val_loss = total_loss / len(loader)
         return model, avg_val_loss