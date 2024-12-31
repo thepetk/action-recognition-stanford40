8d4db0967869486c0d2b4837425e779bcc85fedd
thepetk <thepetk@gmail.com>
2024-12-31 02:18:21 +0000

    Simplifly custom neural network

--- models/custom_nn.py
+++ models/custom_nn.py
@@ -15,6 +15,7 @@ class CustomActionRecogntionNN(nn.Module):
             nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
@@ -22,18 +23,15 @@ class CustomActionRecogntionNN(nn.Module):
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(256, 512),
+            nn.Linear(256, 256),
             nn.ReLU(inplace=True),
             nn.Dropout(0.5),
-            nn.Linear(512, num_classes),
+            nn.Linear(256, num_classes),
         )
 
     def forward(self, x: "torch.Tensor") -> "torch.Tensor":
